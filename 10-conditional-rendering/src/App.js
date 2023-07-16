5a942c6eeb88f400506a104c53ad1f090ee4059e
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-07-16 04:39:54 -0700

    ternary operator

--- 10-conditional-rendering/src/App.js
+++ 10-conditional-rendering/src/App.js
@@ -1,13 +1,14 @@
-import './App.css'
-import PetInfo from './components/PetInfo'
+import "./App.css";
+import PetInfo from "./components/PetInfo";
 
 function App() {
   return (
+    //в первом компоненте  можем не писать булевое значение тк короткая запись ,а false не может быть в ковычках
     <div className="App">
       <PetInfo animal="dog" age={4} hasPet />
       <PetInfo animal="cat" age={7} hasPet={false} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
