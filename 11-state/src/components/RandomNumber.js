a8692978f83d3b604dfb6f14907434b653fd12d4
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-07-17 10:15:45 -0700

    state

--- 11-state/src/components/RandomNumber.js
+++ 11-state/src/components/RandomNumber.js
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import generateRandomNum from '../utils/generateRandomNum'
+import { useState } from "react"; //получаем хук из библиотеки
+import generateRandomNum from "../utils/generateRandomNum";
+function RandomNumber(props) {
+  const { maxNum } = props; //добовляем сво-во
+  const [randomNum, setRandomNum] = useState(generateRandomNum(maxNum)); //деструктуризация, первый элемент радномное число которое будет меняться, второй это функция
 
-function RandomNumber({ maxNum }) {
-  const [randomNum, setRandomNum] = useState(generateRandomNum(maxNum))
   const changeRandomNum = () => {
-    setRandomNum(generateRandomNum(maxNum))
-  }
+    setRandomNum(generateRandomNum(maxNum)); //генерируем случайное число и передаем в фн-ию setRandomNum
+  };
 
   return (
     <div>
       <h1>{randomNum}</h1>
       <button onClick={changeRandomNum}>Generate new random number</button>
     </div>
-  )
+  );
 }
 
-export default RandomNumber
+export default RandomNumber;
+
+//добвляем состояние и по клике на кнопку изменять состояние
+//1 для кнопки добавить св-во OnClick и значение этого
+//св-ва будет функция , которая будет срабатывать при клике
