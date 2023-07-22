ed6ab9bb847b0db4f24186c6d0a5b7def2098d89
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-07-22 16:07:54 -0700

    prop children and style

--- 19-children-prop/src/App.js
+++ 19-children-prop/src/App.js
@@ -1,20 +1,21 @@
-import './App.css'
-import Wrapper from './components/Wrapper'
+import "./App.css";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
       <Wrapper color="lightblue">
         <h2>Text inside of the Wrapper</h2>
-        <button>Click me!</button>
+        <button>Click me</button>
       </Wrapper>
       <Wrapper color="lightgreen">
         <h2>Another text</h2>
-        <p>Some description</p>
+        <p>Some discription me</p>
         <input type="text" placeholder="Enter value" />
       </Wrapper>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+//св-во в компоненте передается через children
