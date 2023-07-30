8296d16d7bcb27337e440b30043f1890182c4272
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-07-30 16:18:01 -0700

    used className and style for NavLink

--- 26-react-router/src/components/Menu.jsx
+++ 26-react-router/src/components/Menu.jsx
@@ -3,16 +3,32 @@ import { NavLink } from "react-router-dom";
 const Menu = () => {
   return (
     <nav>
-      <NavLink to="." end>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: "yellow", textDecoration: "none" } : {}
+        }
+        to="."
+        end>
         Home
       </NavLink>
-      <NavLink to="/contacts"> Contacts</NavLink>
-      <NavLink to="/about"> About</NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "activeLink" : "link")}
+        to="/contacts">
+        Contacts
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "activeLink" : "link")}
+        to="/about">
+        About
+      </NavLink>
     </nav>
   );
 };
 
 export default Menu;
-//NavLink для стилизации пунктов меню
+//NavLink для стилизации пунктов меню + active
 //end для того чтобы предотвратить добавление класса active для домашней страницы
 //при переключении между пунктами меню(т.е мы меняем стиль ссылки только если находимся на стр с корневым путем)
+
+//так же вариант с длбавление класса, фн-ии
+//вариант добавление style
