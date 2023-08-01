856046271ca49a756659aed4e788997161d89f78
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-08-01 03:02:52 -0700

    sort courses

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,11 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { useState } from "react";
 import courses from "../data/courses";
+import { useEffect } from "react";
+
+const SORT_KEYS = ["title", "slug", "id"];
+
+function sortCourses(courses, key) {
+  const sortedCourses = [...courses]; //создали новый массив
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortedCourses;
+  }
+
+  //a,b элементы которые сравнивают
+  sortedCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1)); //получаем доступ к трем св-вам объекта
+  return sortedCourses;
+}
 
 const Courses = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const navigate = useNavigate();
+  const [sortKey, setSortkey] = useState(query.sort);
+  const [sortedCourses, setSortedCourses] = useState(
+    sortCourses(courses, sortKey)
+  );
+
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate("."); //путь ко всем курсам
+      setSortkey(); //обнулили значение sortKey
+      setSortedCourses([...courses]);
+    }
+  }, [sortKey, navigate]);
+
   return (
     <>
-      <h1>Courses</h1>
-      {courses.map((course) => (
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : "Courses"}</h1>
+      {sortedCourses.map((course) => (
         <div>
           key={course.id}
           <Link to={course.slug} className="courseLink">
--- 26-react-router/src/components/SingleCourses.jsx
+++ 26-react-router/src/components/SingleCourses.jsx
@@ -1,14 +1,28 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
+import { useEffect } from "react";
 
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.courseSlug); //ищем определенный курс в массиве курсов
+  useEffect(() => {
+    if (!course) {
+      //направили юзера на стр со всеми курсами
+      navigate("..", { relative: "path" });
+    }
+  }, [course]); //каждый раз когда меняется значение course вызывается фн-ия
+  //если в массиве не указать зависимость course то фн-ия будет вызвана толкьо один раз
+
+  //Simply show NotFound component
+  // if (!course) {
+  //return <NotFound />;
+  // }
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
       <Link to=".." relative="path">
         All courses
       </Link>
@@ -17,3 +31,6 @@ const SingleCourse = () => {
   ); // /courses
 };
 export default SingleCourse;
+
+//useParams - доступ к параметрам пути(после :)
+//useNavigate - перенаправляет юзера на другую страницу
